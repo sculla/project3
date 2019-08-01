fa1d44550a9408a5309f4965b45f5ee9141e4fd2
sculla <sculla@u.washington.edu>
2019-07-31 20:41:51 -0700

    bah

--- feature_eng.py
+++ feature_eng.py
@@ -4,28 +4,46 @@ import pandas as pd
 
 columns = ['ip', 'app', 'device', 'os', 'channel', 'click_time', 'attributed_time', 'is_attributed']
 
+
+def load_csv():
+    x = pd.read_csv('data/train.csv', skiprows=1, names=columns)
+    return x.drop(columns=['attributed_time'])
+
+def tc():
+    x = load_csv()
+    name = 'total_clicks'
+    y = x.groupby(by='ip').count()
+    y = y.reset_index()
+    y = y.rename(columns={'channel': f'{name}'})
+    y = y[['ip',f'{name}']]
+    y.to_csv(f'data/ip_{name}.csv')
+
 def c_s():
     # clicks / seconds
+    x = load_csv()
+    name = 'clicks_sec'
     y = x.groupby(by=['ip', 'click_time']).count()
     y = y.reset_index()
-    x = x.merge(y[['ip', 'n_clicks']], on='ip', how='left')
-    x = x.rename(columns={'n_clicks_y': 'clicks_sec', 'n_clicks_x': 'n_clicks'})
+    y = y.rename(columns={'channel': f'{name}'})
+    y = y[['ip','click_time',f'{name}']]
+    y.to_csv(f'data/ip_{name}.csv')
 
 def dl():
     # total downloads
+    x = load_csv()
+    name = 'total_downloads'
     y = x.groupby(by=['ip', 'is_attributed']).count()
     y = y.reset_index()
-    x = x.merge(y[['ip', 'is_attributed']], on='ip', how='left')
-    x = x.rename(columns={'is_attributed_y': 'total_downloads', 'is_attributed_x': 'is_attributed'})
+    y = y.rename(columns={'channel': f'{name}'})
+    y = y[['ip',f'{name}']]
+    y.to_csv(f'data/ip_{name}.csv')
 
 if __name__ == '__main__':
-    x = pd.read_csv('data/train.csv', skiprows=1, names=columns)
-    x.drop(columns=['attributed_time'], inplace=True)
+    print('start')
 
-    # total clicks by ip
-    y = x.groupby(by='ip').count()
-    y = y.reset_index()
-    # x = x.merge(y[['ip','click_time']], on='ip', how='left')
-    # x = x.rename(columns={'click_time_y': 'n_clicks','click_time_x': 'click_time'})
+    print('mid')
+    c_s()
+    print('done')
+
 
 
